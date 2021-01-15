20fd7414f19f681008a7eb4a72c25d1560dd869c
LOUNI ENZO p1818982 <enzo.louni@etu.univ-lyon1.fr>
2021-01-15 16:36:27 +0100

    Commit_Antonin

---
+++ Logiciel Devis-Facture/packModele/Logo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreationDatabase
+{
+    class Logo
+    {
+        private string name;
+        private string format;
+        private int height;
+        private int width;
+        private string pathing;
+
+        Logo()
+        {
+            ;
+        }
+        Logo(string _name,string _format, int _height, int _width, string _pathing)
+        {
+            name = _name;
+            format = _format;
+            height = _height;
+            width = _width;
+            pathing = _pathing;
+        }
+
+        void setName(string a) {
+            name = a;
+        };
+        void setFormat(string a){
+            format = a;
+        };
+        void setPathing(string a) {
+            pathing = a;
+        }
+        void setWidth(int a) {
+            width = a;
+        };
+        void setHeight(int a){
+            height = a;
+        };
+
+        string getName()
+        {
+            return name;
+        };
+        string getFormat()
+        {
+            return format;
+        };
+        string getPathing()
+        {
+            return pathing;
+        };
+        int getHeight()
+        {
+            return height;
+        }
+        int getWidth()
+        {
+            return width;
+        }
+    }
+}
