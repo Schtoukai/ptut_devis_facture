4645a80b4bfbfe4ecad03cf4499aaa1191090654
LOUNI ENZO p1818982 <enzo.louni@etu.univ-lyon1.fr>
2021-01-18 14:57:19 +0100

    commit(creation PDF)

---
+++ Logiciel Devis-Facture/packForm/Create_PDF.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logiciel_Devis_Facture
+{
+    public partial class Create_PDF : Form
+    {
+        public Create_PDF()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonValidate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
